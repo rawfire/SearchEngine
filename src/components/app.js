81dae02b6b7013102b6aee00ed14461214fc6f1f
Ignand <ignatius.and@gmail.com>
2020-01-04 15:55:16 -0500

    style the home with CSS grid

--- src/components/app.js
+++ src/components/app.js
@@ -16,7 +16,7 @@ class App extends Component {
 }
   render() {
     return (
-      <div>
+      <div className="home">
         <Logo/>
         <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
         <RecentPosts/>
