c00bd75b4a88d71c15bb0069d363932a375573af
Ignand <ignatius.and@gmail.com>
2020-01-04 13:13:55 -0500

    modified post component to render a result post and setup render links

--- src/components/posts.js
+++ src/components/posts.js
@@ -7,8 +7,25 @@ export default class Post extends Component {
         })
         return topics;
     }
+
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index} >
+                    <div className="post-link-box"> 
+                    </div>
+                    <div className="post-link-link">
+                        <a href={post_link.link_url} > Useful Link #{index + 1} </a>
+                    </div>
+                </div>
+            )
+        })
+        return links;
+    }
+
     render() {
-        return (
+        if(this.props.type === "recent") {
+            return (
                 <li className="recent-posts">
                     <div className="recent-post-title">
                         {this.props.title}
@@ -17,7 +34,23 @@ export default class Post extends Component {
                         {this.renderTopics()}
                     </div>
                 </li>
-        )
+            )
+        } else if (this.props.type === "result") {
+            return (
+                <li className="result-post">
+                    <div className="result-post-topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post-title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post-links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
+
     }
 }
 
